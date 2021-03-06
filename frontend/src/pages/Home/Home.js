4a3a4f6751b5a3f1a38611b9123713c73c995e97
retroverse <ebreakey@gmail.com>
2021-03-06 21:33:41 +1100

    Implement goal fetching on Home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,18 +1,38 @@
 import { useHistory } from 'react-router-dom';
-
+import { useState, useEffect } from 'react';
 import { Button } from 'components';
 
 import {
   Container,
   Title,
 } from './homeStyle';
+import { getAuthoredGoals } from '../../services/api'
+
+const sum = (a, b) => a + b
 
 const Home = () => {
   const { push } = useHistory();
 
+  const [userID, _] = useState(1);
+  const [goals, setGoals] = useState();
+
+  // refetch goals on userID update
+  useEffect(() => {
+      getAuthoredGoals(userID).then(goals => {
+          console.log(goals)
+          setGoals(goals)
+      }).catch(err => console.warn(err))
+  }, [userID])
+
   return (
     <Container>
       <Title>Home</Title>
+      <div>
+        <span>
+        Total current pledged
+        </span>
+        <h3>{goals ? goals.map(g => g.amount).reduce(sum) : 0}$</h3>
+      </div>
       <p>Henlo says gencko</p>
       <Button type="button" onClick={() => push('/goal/test')}>View test goal</Button>
       <Button type="button" onClick={() => push('/new')}>Create a new goal</Button>
