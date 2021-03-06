a59be6dfdd5df3e24f3f5ce50fef1586d5dfbf93
retroverse <ebreakey@gmail.com>
2021-03-07 03:09:06 +1100

    Fix field name in getGoal route

--- backend/src/routes/getGoal.ts
+++ backend/src/routes/getGoal.ts
@@ -2,8 +2,8 @@ import * as db from '../util/db'
 
 export const getGoal = async (req, res) => {
     try {
-        const user = await db.getGoal(req.query.id)
-        return res.json({ success: true, user })
+        const goal = await db.getGoal(req.query.id)
+        return res.json({ success: true, goal })
     } catch (error) {
         if (error.name == db.NO_SUCH_ROW_ERR) {
             return res.status(404).json({ success: false, error })
